26795140d01d24d73bd519a41824acbcc71c82ea
arthur <rlarlaks21c@naver.com>
2020-12-30 14:35:32 +0900

    eslint, prettier 적용

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never', jsx: 'never', ts: 'never', tsx: 'never', json: 'never',
+    }],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+  },
+  settings: { 'import/resolver': { node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] } } },
+};
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # study-redux-saga
+
+## eslint 적용
+`npm install -D eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser`
+
+### airbnb 규칙 적용
+`npx install-peerdeps --dev eslint-config-airbnb`
+
+## prettier 적용
+`npm install -D prettier eslint-config-prettier eslint-plugin-prettier`
